959bb8c5e22608b61da7a5708bc6d033267de69a
Khosi <mahdi0khosravi@gmail.com>
2019-04-27 21:44:21 +0430

    form tor added

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,26 +1,199 @@
 import React from "react";
-import logo from "../../static/img/logo.svg";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <Container>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Container className="text-center">
+      <Row>
+        <Col className="border">نام مشتری</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border" />
+        <Col className="border">کد مشتری</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          اولویت مشتری
+        </Col>
+        <Col className="border" />
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">کد اقتصادی</Col>
+        <Col className="border" lg="3" md="3" sm="3" xl="3" xs="3">
+          ---
+        </Col>
+        <Col className="border">شناسه ملی</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          شماره ثیت
+        </Col>
+        <Col className="border">---</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2" />
+      </Row>
+
+      <Row>
+        <Col className="border">آدرس دفتر</Col>
+        <Col className="border" lg="4" md="4" sm="4" xl="4" xs="4">
+          ---
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          تلفن
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">فکس</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">آدرس کارخانه</Col>
+        <Col className="border" lg="4" md="4" sm="4" xl="4" xs="4">
+          ---
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          آدرس ایمیل
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">آدرس سایت</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">کد پستی</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border" />
+        <Col className="border">مدیر عامل</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          تلفن مدیرعامل
+        </Col>
+        <Col className="border">---</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2" />
+      </Row>
+
+      <Row>
+        <Col className="border">مدیر بازرگانی</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">مدیر تدارکات</Col>
+        <Col className="border">---</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          مدیر تعمیرات
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">کارشناس</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">تلفن بازرگانی</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">تلفن تدارکات</Col>
+        <Col className="border">---</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          تلفن تعمیرات
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">تلفن کارشناس</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">ایمیل بازرگانی</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">ایمیل تدارکات</Col>
+        <Col className="border">---</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ایمیل تعمیرات
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">ایمیل کارشناس</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">نوع مالکیت</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">نوع فعالیت</Col>
+        <Col className="border">---</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          طبقه‌بندی مشتری
+        </Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">نحوه‌ی آشنایی</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">سابقه‌ی خرید</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          ---
+        </Col>
+        <Col className="border">نوع خرید</Col>
+        <Col className="border">---</Col>
+        <Col className="border" lg="2" md="2" sm="2" xl="2" xs="2">
+          حجم خرید ریالی
+        </Col>
+        <Col className="border" lg="5" md="5" sm="5" xl="5" xs="5">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">توضیحات خرید</Col>
+        <Col className="border" lg="11" md="11" sm="11" xl="11" xs="11">
+          ---
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="border">توضیحات درباره‌ی مشتری</Col>
+        <Col className="border" lg="11" md="11" sm="11" xl="11" xs="11">
+          ---
+        </Col>
+      </Row>
     </Container>
   );
 }
