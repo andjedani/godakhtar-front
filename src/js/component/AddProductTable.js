51bb35c107ebbd9420c697b01e623c5a6ac2909e
Khosi <mahdi0khosravi@gmail.com>
2019-06-13 21:38:54 +0430

    get product types added

--- src/js/component/AddProductTable.js
+++ src/js/component/AddProductTable.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Table, Select, Button, Row, Col, Input, Card } from "antd";
+import { Table, Select, Button, Row, Col, Card } from "antd";
+import { productOptions } from "../network";
 
 const { Option } = Select;
 
@@ -50,7 +51,14 @@ const data = [
 ];
 
 class AddProductTable extends React.Component {
-  state = { data: [], initloading: false, num: 1 };
+  state = {
+    data: [],
+    initloading: false,
+    product_class: [],
+    product_connection: [],
+    product_size: [],
+    product_type: []
+  };
 
   expandedRowRender = customItems => {
     const columns = [];
@@ -68,20 +76,22 @@ class AddProductTable extends React.Component {
     );
   };
 
-  render() {
-    const customs = [];
-    for (let i = 0; i < this.state.num; i++) {
-      customs.push(
-        <Row justify="center" key={i}>
-          <Col span={2} style={{ textAlign: "center", padding: "20px" }}>
-            <Input placeholder="نام" />
-          </Col>
-          <Col span={4} style={{ textAlign: "center", padding: "20px" }}>
-            <Input placeholder="توضیحات" />
-          </Col>
-        </Row>
-      );
+  componentDidMount = async () => {
+    let data = await productOptions();
+    if (data.data.actions.POST) {
+      data = data.data.actions.POST;
+      this.setState({
+        product_class: data.product_class.choices,
+        product_connection: data.product_size.choices,
+        product_size: data.product_type.choices,
+        product_type: data.product_connection.choices
+      });
     }
+  };
+
+  render() {
+    let attributes = [];
+
     return (
       <div>
         <Card>
@@ -91,12 +101,11 @@ class AddProductTable extends React.Component {
             </Col>
             <Col span={4} style={{ textAlign: "center" }}>
               <Select style={{ width: 200 }}>
-                <Option value="jack">Jack</Option>
-                <Option value="lucy">Lucy</Option>
-                <Option value="disabled" disabled>
-                  Disabled
-                </Option>
-                <Option value="Yiminghe">yiminghe</Option>
+                {this.state.product_type.map(item => (
+                  <Option value={item.value} key={item.value}>
+                    {item.display_name}
+                  </Option>
+                ))}
               </Select>
             </Col>
 
@@ -105,12 +114,11 @@ class AddProductTable extends React.Component {
             </Col>
             <Col span={4} style={{ textAlign: "center" }}>
               <Select style={{ width: 200 }}>
-                <Option value="jack">Jack</Option>
-                <Option value="lucy">Lucy</Option>
-                <Option value="disabled" disabled>
-                  Disabled
-                </Option>
-                <Option value="Yiminghe">yiminghe</Option>
+                {this.state.product_class.map(item => (
+                  <Option value={item.value} key={item.value}>
+                    {item.display_name}
+                  </Option>
+                ))}
               </Select>
             </Col>
 
@@ -119,12 +127,11 @@ class AddProductTable extends React.Component {
             </Col>
             <Col span={4} style={{ textAlign: "center" }}>
               <Select style={{ width: 200 }}>
-                <Option value="jack">Jack</Option>
-                <Option value="lucy">Lucy</Option>
-                <Option value="disabled" disabled>
-                  Disabled
-                </Option>
-                <Option value="Yiminghe">yiminghe</Option>
+                {this.state.product_connection.map(item => (
+                  <Option value={item.value} key={item.value}>
+                    {item.display_name}
+                  </Option>
+                ))}
               </Select>
             </Col>
             <Col span={2} style={{ textAlign: "center" }}>
@@ -132,18 +139,27 @@ class AddProductTable extends React.Component {
             </Col>
             <Col span={4} style={{ textAlign: "center" }}>
               <Select style={{ width: 200 }}>
-                <Option value="jack">Jack</Option>
-                <Option value="lucy">Lucy</Option>
-                <Option value="disabled" disabled>
-                  Disabled
-                </Option>
-                <Option value="Yiminghe">yiminghe</Option>
+                {this.state.product_size.map(item => (
+                  <Option value={item.value} key={item.value}>
+                    {item.display_name}
+                  </Option>
+                ))}
               </Select>
             </Col>
           </Row>
 
-          {customs}
+          <br />
 
+          <Row>
+            <Col>
+              <Button type="dashed" onClick={() => {}}>
+                دریافت ویژگی‌ها
+              </Button>
+            </Col>
+          </Row>
+          {attributes}
+
+          <br />
           <Button type="primary" onClick={() => {}}>
             افزودن محصول درخواستی
           </Button>
---
+++ src/js/network/product.js
@@ -0,0 +1,14 @@
+import { baseUrl, api } from ".";
+import axios from "axios";
+
+const serviceUrl = baseUrl + api + "products/";
+
+export const productOptions = () => {
+  const options = {
+    method: "OPTIONS",
+    url: serviceUrl
+  };
+  return axios(options).catch(function(error) {
+    console.log(error);
+  });
+};
