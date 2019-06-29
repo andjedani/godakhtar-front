688df8aa233360f0ed8fe2f150dd1d06666f54ca
Khosi <mahdi0khosravi@gmail.com>
2019-06-29 20:21:04 +0430

    some step changes

--- src/js/component/BusinessProperties.js
+++ src/js/component/BusinessProperties.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Layout, Card, Col, Row } from "antd";
+import { Layout, Input, Row, Col, Card, Radio } from "antd";
+import { DatePicker } from "jalali-react-datepicker";
+
+const radioStyle = {
+  display: "block",
+  height: "30px",
+  lineHeight: "30px"
+};
 
 class BusinessProperties extends React.Component {
   render() {
@@ -12,23 +19,54 @@ class BusinessProperties extends React.Component {
                 title="شرایط پرداخت"
                 bordered={false}
                 style={{ width: 300 }}
-              />
+              >
+                <Radio.Group>
+                  <Radio style={radioStyle} value={0}>
+                    به صورت ۵۰٪ پیش پرداخت - الباقی قبل از حمل
+                  </Radio>
+                  <Radio style={radioStyle} value={1}>
+                    به صورت ۲۵٪ پیش پرداخت و الباقی بعد از تحویل کالا
+                  </Radio>
+                </Radio.Group>
+              </Card>
             </Col>
             <Col span={12}>
-              <Card
-                title="زمان تحویل"
-                bordered={false}
-                style={{ width: 300 }}
-              />
+              <span
+                style={{
+                  float: "right",
+                  border: "1px solid #d9d9d9",
+                  borderRadius: "4px",
+                  padding: "5px"
+                }}
+              >
+                تاریخ تحویل
+              </span>
+              <span>
+                <DatePicker
+                  timePicker={false}
+                  onClickSubmitButton={value => console.log(value["_i"])}
+                />
+              </span>
             </Col>
           </Row>
-          <br />
-          <Row gutter={16}>
+          <Row>
             <Col span={12}>
-              <Card title="محل تحویل" bordered={false} style={{ width: 300 }} />
+              <Card title="محل تحویل" bordered={false} style={{ width: 300 }}>
+                <Radio.Group>
+                  <Radio style={radioStyle} value={0}>
+                    درب انبار گداختار - اراک
+                  </Radio>
+                  <Radio style={radioStyle} value={1}>
+                    درب انبار مشتری
+                  </Radio>
+                  <Radio style={radioStyle} value={2}>
+                    پشت گمرک
+                  </Radio>
+                </Radio.Group>
+              </Card>
             </Col>
             <Col span={12}>
-              <Card title="وندورلیست" bordered={false} style={{ width: 300 }} />
+              <Input addonBefore="وندورلیست" />
             </Col>
           </Row>
         </Card>
